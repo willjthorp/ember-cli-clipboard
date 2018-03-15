61870ae42ce2bad44467334522e2c4f4dffa2f59
martinmalinda <malindacz@gmail.com>
2018-03-15 13:50:33 +0100

    remove importing of clipboardjs

--- index.js
+++ index.js
@@ -5,20 +5,5 @@ var path = require('path');
 var fastbootTransform = require('fastboot-transform');
 
 module.exports = {
-  name: 'ember-cli-clipboard',
-
-  treeForVendor() {
-    var Funnel = require('broccoli-funnel');
-    var clipboardPath = path.join(path.dirname(require.resolve('clipboard')), '..');
-
-    return new fastbootTransform(Funnel(this.treeGenerator(clipboardPath), {
-      destDir: 'clipboard'
-    }));
-  },
-
-  included() {
-    this._super.included.apply(this, arguments);
-
-    this.import('vendor/clipboard/dist/clipboard.js');
-  }
+  name: 'ember-cli-clipboard'
 };
